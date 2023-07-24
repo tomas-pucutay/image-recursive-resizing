22a986316b748f67680846b9bf4893c8b28a1823
tomas-pucutay <tomas.pucutay@gmail.com>
2023-07-24 00:01:46 -0500

    Added libraries and main function to main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import glob
+import os
+import sys
+
+def redim_image(input_path, required_width):
+    pass
+
+if __name__=="__main__":
+    input_path = False
+    required_width = False
+    redim_image(input_path, required_width)
