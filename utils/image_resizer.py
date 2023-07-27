a1ed8594ad10feeb21cba586a2d8007c0ac3e726
tomas-pucutay <tomas.pucutay@gmail.com>
2023-07-27 11:52:28 -0500

    Import image test with Pillow

--- utils/image_resizer.py
+++ utils/image_resizer.py
@@ -2,4 +2,6 @@ import os
 from PIL import Image
 
 def resize_image(image_path, required_width):
-    return None
+    # Open image with Pillow
+    image_pil = Image.open(image_path)
+    return image_pil
