c839c5682e9a94232ecf8e66d15197afa09be24f
tomas-pucutay <tomas.pucutay@gmail.com>
2023-07-27 11:58:08 -0500

    Added the structure to call the imagen and output paths from main

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from utils.image_resizer import resize_image
 from utils.file_handler import find_images, create_output_folder, build_output_path
 
 def redim_image(input_path, required_width):
-    pass
+    images = find_images(input_path)
+    output_path = create_output_folder(input_path)
 
 if __name__=="__main__":
     input_path = None
