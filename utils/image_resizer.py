1c08fcebf2c9671c74f1cff154b3f6769acb987c
tomas-pucutay <tomas.pucutay@gmail.com>
2023-07-27 11:54:29 -0500

    Resizing image to a desired width keeping aspect ratio

--- utils/image_resizer.py
+++ utils/image_resizer.py
@@ -4,4 +4,14 @@ from PIL import Image
 def resize_image(image_path, required_width):
     # Open image with Pillow
     image_pil = Image.open(image_path)
-    return image_pil
+
+    # Resize image to desired width and keep aspect ratio
+    width, height = image_pil.size
+    if width > required_width:
+        proportion = required_width / width
+        required_height = int(height * proportion)
+        resized_image = image_pil.resize((required_width, required_height), Image.LANCZOS)
+    else:
+        resized_image = image_pil
+        
+    return resized_image
