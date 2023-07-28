3f6c1d152ef8c17c767b3e076403965f72711697
tomas-pucutay <tomas.pucutay@gmail.com>
2023-07-28 12:00:32 -0500

    Added code to main to resize and save image

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ def redim_image(input_path, required_width):
     images = find_images(input_path)
     output_path = create_output_folder(input_path)
 
+    for image in images:
+        image_output_path = build_output_path(image, input_path, output_path)
+
+        # Resize image and get image_info
+        resized_image, image_info = resize_image(image, required_width)
+
+        if resized_image is not None:
+            # Compress and save the resized image without the metadata
+            resized_image.save(image_output_path, optimize=True, quality=85, **image_info)
+            print(f'done {resized_image}')
+
 if __name__=="__main__":
     input_path = None
     required_width = None
