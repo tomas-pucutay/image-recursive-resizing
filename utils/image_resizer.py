857733bc91280a8d13dedcf6f1f30277f32c0ada
tomas-pucutay <tomas.pucutay@gmail.com>
2023-07-28 12:05:32 -0500

    Added exception block to image_resizer

--- utils/image_resizer.py
+++ utils/image_resizer.py
@@ -2,20 +2,25 @@ import os
 from PIL import Image
 
 def resize_image(image_path, required_width):
-    # Open image with Pillow
-    image_pil = Image.open(image_path)
+    try:
+        # Open image with Pillow
+        image_pil = Image.open(image_path)
 
-    # Resize image to desired width and keep aspect ratio
-    width, height = image_pil.size
-    if width > required_width:
-        proportion = required_width / width
-        required_height = int(height * proportion)
-        resized_image = image_pil.resize((required_width, required_height), Image.LANCZOS)
-    else:
-        resized_image = image_pil
+        # Resize image to desired width and keep aspect ratio
+        width, height = image_pil.size
+        if width > required_width:
+            proportion = required_width / width
+            required_height = int(height * proportion)
+            resized_image = image_pil.resize((required_width, required_height), Image.LANCZOS)
+        else:
+            resized_image = image_pil
 
-    # Remove image metadata
-    image_info = resized_image.info.copy()
-    image_info.pop("exif", None)
+        # Remove image metadata
+        image_info = resized_image.info.copy()
+        image_info.pop("exif", None)
 
-    return resized_image, image_info
+        return resized_image, image_info
+    
+    except (OSError, IOError, Image.UnidentifiedImageError):
+        # Return None if image cannot be identified
+        return None, None
