6cdd3069eaf73f277389f3265496cc4f28386b46
tomas-pucutay <tomas.pucutay@gmail.com>
2023-07-28 12:03:57 -0500

    Added code to manage exceptions from main and arguments in the main function

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ def redim_image(input_path, required_width):
     output_path = create_output_folder(input_path)
 
     for image in images:
-        image_output_path = build_output_path(image, input_path, output_path)
+        try:
+            image_output_path = build_output_path(image, input_path, output_path)
 
-        # Resize image and get image_info
-        resized_image, image_info = resize_image(image, required_width)
+            # Resize image and get image_info
+            resized_image, image_info = resize_image(image, required_width)
 
-        if resized_image is not None:
-            # Compress and save the resized image without the metadata
-            resized_image.save(image_output_path, optimize=True, quality=85, **image_info)
-            print(f'done {resized_image}')
+            if resized_image is not None:
+                # Compress and save the resized image without the metadata
+                resized_image.save(image_output_path, optimize=True, quality=85, **image_info)
+                print(f'done {resized_image}')
+        
+        except Exception as e:
+            # Skip images that cannot be processed
+            print(f"Error processing image: {image}")
+            print(e)
+            continue
 
+# Input folder path and output folder
 if __name__=="__main__":
-    input_path = None
-    required_width = None
+    input_path = os.path.join(os.getcwd(), 'images')
+    required_width = 1200
     redim_image(input_path, required_width)
